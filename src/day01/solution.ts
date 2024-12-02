9525ba3a796cf1b6a488d12e4de7763d2225e290
Clément Pasek <clement.pasek@playplay.com>
2024-12-02 11:42:11 +0100

    feat: day 1

--- src/day01/solution.ts
+++ src/day01/solution.ts
@@ -4,14 +4,58 @@ export function readInput(filePath: string): string {
     return readFileSync(filePath, "utf-8").trim();
 }
 
+export function createArraysFromInput(input: string): { left: number[], right: number[] } {
+   return input.split('\n').reduce<{ left: number[], right: number[] }>((accumulator, currentValue) => {
+        const [left, right] = currentValue.split('   ')!;
+        accumulator.left.push(parseInt(left));
+        accumulator.right.push(parseInt(right));
+        return accumulator;
+    }, {
+        left: [],
+        right: [],
+    })
+}
+
 export function solvePart1(input: string): number {
-    console.warn('bonjour', input)
-    return 0;
+    const { left, right } = createArraysFromInput(input);
+    const sortedLeft = left.sort((a, b) => a - b);
+    const sortedRight = right.sort((a, b) => a - b);
+
+    let counter = 0
+    for (let i = 0; i < sortedLeft.length; i++) {
+        counter += Math.abs(sortedLeft[i] - sortedRight[i]);
+    }
+    return counter;
+}
+
+export function createArraysOfOccurences(array: number[]): [{ nbr: number, occurence: number }] {
+    const { left, right } = createArraysFromInput(input);
+    return []
 }
 
 export function solvePart2(input: string): number {
-    // Implémente ici la solution pour la partie 2
-    return 0;
+    const { left, right } = createArraysFromInput(input);
+    const arraysOfOccurences: { nbr: number, occurence: number }[] = []
+
+    for (let i = 0; i < left.length; i++) {
+        const currentNbrWithOccurences = arraysOfOccurences.find(array => array.nbr === left[i])
+        // Don't need to loop the right list again
+        if (currentNbrWithOccurences) {
+            arraysOfOccurences.push(currentNbrWithOccurences)
+        } else {
+            const nbrWithOccurences: { nbr: number, occurence: number } = { nbr: left[i], occurence: 0 }
+            right.forEach(rightNbr => {
+                if (left[i] === rightNbr) {
+                    nbrWithOccurences.occurence++
+                }
+            })
+            arraysOfOccurences.push(nbrWithOccurences)
+        }
+    }
+
+    return arraysOfOccurences.reduce<number>((accumulator, currentValue) => {
+        return accumulator + currentValue.nbr * currentValue.occurence
+    }, 0)
 }
 
 // Si le fichier est exécuté directement (par exemple via `bun run` ou `node`), lis l'entrée et affiche les résultats
--- tests/day01.test.ts
+++ tests/day01.test.ts
@@ -1,14 +1,29 @@
-import { describe, it, expect } from "bun:test";
-import { solvePart1, solvePart2 } from "@/day01/solution";
+import { describe, it, expect, test } from "bun:test";
+import { solvePart1, solvePart2, createArraysFromInput } from "@/day01/solution";
 
 describe("Day 1", () => {
-    it("Part 1 example", () => {
-        const exampleInput = "example input";
-        expect(solvePart1(exampleInput)).toBe(0);
-    });
+    describe('createArraysFromInput', () => {
+        test('It should create two arrays', () => {
+            expect(createArraysFromInput('3   4\n4   3\n2   5')).toEqual({ left: [3, 4, 2], right: [4, 3, 5] })
+        })
+    })
 
-    it("Part 2 example", () => {
-        const exampleInput = "example input";
-        expect(solvePart2(exampleInput)).toBe(0);
+    describe('Part 1', () => {
+        test("It should return the correct answer", () => {
+            const exampleInput = '3   4\n4   3\n2   5';
+            expect(solvePart1(exampleInput)).toBe(3);
+        });
+    
+        test("It should return the correct answer", () => {
+            const exampleInput = `3   4\n4   3\n2   5\n1   3\n3   9\n3   3`;
+            expect(solvePart1(exampleInput)).toBe(11);
+        });
     });
+    
+    describe('Part 2', () => {
+        test('It should return the correct answer', () => {
+            const exampleInput = `3   4\n4   3\n2   5\n1   3\n3   9\n3   3`;
+            expect(solvePart2(exampleInput)).toBe(31);
+        })
+    })
 });
